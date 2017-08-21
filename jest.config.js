ffe9c4ea87ce4839bfaa3d0dd2f20bce5ff4df7e
Mark Miyashita <negativetwelve@gmail.com>
2017-08-21 01:53:31 -0700

    Remove jest config since it was not being used

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // TODO(mark): This is currently being ignored:
-  // https://github.com/facebook/jest/issues/3718
-  projects: [
-    '<rootDir>/examples/*',
-    '<rootDir>/projects/*',
-  ],
-  testEnvironment: 'node',
-};
