67c080717d452a177e95038602d6acfdcc699cbb
Mark Miyashita <negativetwelve@gmail.com>
2017-08-21 03:52:24 -0700

    Create setup file for jest-plugins-recommended

---
+++ packages/jest-plugins-recommended/setup.js
@@ -0,0 +1,9 @@
+// Sets up the recommended set of plugins.
+require('jest-plugins')([
+  'jest-plugin-action',
+  'jest-plugin-console-matchers',
+  'jest-plugin-context',
+  'jest-plugin-its',
+  'jest-plugin-set',
+  'jest-plugin-unhandled-promise',
+]);
