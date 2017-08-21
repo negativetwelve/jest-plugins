b1c03d49d9908fd5d3e5c019a5d7e7ca58128104
Mark Miyashita <negativetwelve@gmail.com>
2017-08-20 23:29:38 -0700

    Add top-level jest config with support for packages

--- tasks/setup-examples.sh
+++
@@ -1,3 +0,0 @@
-# Augment this as we add more examples.
-cd ./examples/basic && yarn && cd ../..
-cd ./examples/plugin-installation && yarn && cd ../..
--- tasks/test-examples.sh
+++
@@ -1,3 +0,0 @@
-# Augment this as we add more examples.
-cd ./examples/basic && yarn test && cd ../..
-cd ./examples/plugin-installation && yarn test && cd ../..
