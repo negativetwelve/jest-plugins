8a5fd99c7bcc495b54a795b5566144368d34b876
Mark Miyashita <negativetwelve@gmail.com>
2017-08-28 21:30:54 -0700

    Add basic frame for all specs for the clock plugin

---
+++ packages/jest-plugin-clock/src/__tests__/clock.spec.js
@@ -0,0 +1,70 @@
+// Modules
+import clock from '../clock';
+
+
+/* eslint-disable no-undef */
+describe('clock', () => {
+  context('with string date', () => {
+    clock.set('');
+
+    it('returns the correct year', () => {
+
+    });
+
+    it('returns the correct month', () => {
+
+    });
+
+    it('returns the correct day', () => {
+
+    });
+
+    it('returns the correct hour', () => {
+
+    });
+
+    it('returns the correct minute', () => {
+
+    });
+
+    it('returns the correct seconds', () => {
+
+    });
+
+    it('returns the correct milliseconds', () => {
+
+    });
+  });
+
+  context('with date object', () => {
+    clock.set(new Date());
+
+    it('returns the correct year', () => {
+
+    });
+
+    it('returns the correct month', () => {
+
+    });
+
+    it('returns the correct day', () => {
+
+    });
+
+    it('returns the correct hour', () => {
+
+    });
+
+    it('returns the correct minute', () => {
+
+    });
+
+    it('returns the correct seconds', () => {
+
+    });
+
+    it('returns the correct milliseconds', () => {
+
+    });
+  });
+});
