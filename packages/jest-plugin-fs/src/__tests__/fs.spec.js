3ef5692f22b1fed5c4ed8e0c2104cfc8e6ab3c40
Mark Miyashita <negativetwelve@gmail.com>
2017-09-02 04:32:14 -0700

    Create basic tests for the jest-plugin-fs module

---
+++ packages/jest-plugin-fs/src/__tests__/fs.spec.js
@@ -0,0 +1,20 @@
+// Modules
+import fsExtra from 'fs-extra';
+import set from 'jest-plugin-set';
+import fs from '../fs';
+
+
+/* eslint-disable no-undef */
+describe('fs', () => {
+  beforeEach(() => fs.create({}));
+  afterEach(() => fs.restore());
+
+  describe('new file', () => {
+    set('filename', () => 'some/path/to/file.txt');
+    beforeEach(() => fsExtra.outputFileSync(filename, 'test-content'));
+
+    it('should create the new file', () => {
+      expect(fsExtra.readFileSync(filename, 'utf8')).toEqual('test-content');
+    });
+  });
+});
