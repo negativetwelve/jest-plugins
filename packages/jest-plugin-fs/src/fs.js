9cef1d8cb60502ac008b5724b61b4cb1eab2d7be
Mark Miyashita <negativetwelve@gmail.com>
2017-09-22 15:52:43 -0700

    Reset the filesystem before mocking again

--- packages/jest-plugin-fs/src/fs.js
+++ packages/jest-plugin-fs/src/fs.js
@@ -3,8 +3,23 @@ import {vol} from 'memfs';
 
 
 const fs = {
-  mock: (filesystem = {}) => vol.fromJSON(filesystem, '/'),
+  /**
+   * When we mock the filesystem, we need to first reset, then create an
+   * instance from JSON.
+   */
+  mock: (filesystem = {}) => {
+    vol.reset();
+    vol.fromJSON(filesystem, '/');
+  },
+
+  /**
+   * Returns a JS object with the mocked filesystem contents.
+   */
   read: () => vol.toJSON(),
+
+  /**
+   * Restores the filesystem and removes all existing files.
+   */
   restore: () => vol.reset(),
 };
 
